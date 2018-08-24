8e3ef2774b0942123c7f840077c615c737dea3ca
pkse1213 <pkse1213@naver.com>
2018-08-25 05:39:26 +0900

    commit

---
+++ KEPCOde/View/PartternCell.swift
@@ -0,0 +1,13 @@
+//
+//  PartternCell.swift
+//  KEPCOde
+//
+//  Created by 박세은 on 2018. 8. 25..
+//  Copyright © 2018년 박세은. All rights reserved.
+//
+
+import UIKit
+
+class PartternCell: UICollectionViewCell {
+    
+}
