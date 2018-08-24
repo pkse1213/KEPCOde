2d1a760d288212a0d00ec6c7515607bb879c8c10
pkse1213 <pkse1213@naver.com>
2018-08-24 14:09:08 +0900

    made project

---
+++ KEPCOde/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  KEPCOde
+//
+//  Created by 박세은 on 2018. 8. 24..
+//  Copyright © 2018년 박세은. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
